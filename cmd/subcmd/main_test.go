6566bd9749d37a717111131c1d26dbb5315c3c49
winebarrel <sugawara@winebarrel.jp>
2026-02-08 18:54:24 +0900

    Use httpmock for subcmd test

--- cmd/subcmd/main_test.go
+++ cmd/subcmd/main_test.go
@@ -3,18 +3,12 @@ package subcmd_test
 import (
 	"os"
 	"testing"
-	"time"
-
-	"github.com/winebarrel/s3lock"
 )
 
 func TestMain(m *testing.M) {
 	os.Setenv("AWS_ACCESS_KEY_ID", "test")
 	os.Setenv("AWS_SECRET_ACCESS_KEY", "test")
-	os.Setenv("AWS_DEFAULT_REGION", "us-east-1")
-	os.Setenv("AWS_ENDPOINT_URL", "http://localhost:9090")
-
-	s3lock.LockWaitInterval = 100 * time.Millisecond
+	os.Setenv("AWS_REGION", "us-east-1")
 
 	m.Run()
 }
--- cmd/subcmd/unlock_test.go
+++ cmd/subcmd/unlock_test.go
@@ -2,50 +2,27 @@ package subcmd_test
 
 import (
 	"bytes"
+	"io"
+	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/require"
 	"github.com/winebarrel/s3lock"
 	"github.com/winebarrel/s3lock/cmd/subcmd"
 )
 
 func TestUnlockCmd(t *testing.T) {
-	s3cli := testNewS3Client(t)
-	testDeleteObject(t, s3cli, "s3lock-test", "lock-obj")
+	hc := &http.Client{}
+	httpmock.ActivateNonDefault(hc)
+	t.Cleanup(func() { httpmock.DeactivateNonDefault(hc) })
 
-	obj := s3lock.New(s3cli, "s3lock-test", "lock-obj")
-	lock, err := obj.Lock(t.Context())
-	require.NoError(t, err)
-
-	j, err := lock.MarshalJSON()
-	require.NoError(t, err)
-	lockInfo := filepath.Join(t.TempDir(), "lock.info")
-	err = os.WriteFile(lockInfo, j, 0600)
-	require.NoError(t, err)
-
-	cmd := &subcmd.UnlockCmd{
-		LockFile: lockInfo,
-	}
-
-	var buf bytes.Buffer
-
-	err = cmd.Run(&subcmd.Context{
-		S3:     s3cli,
-		Output: &buf,
-	})
-
-	require.NoError(t, err)
-	require.Empty(t, buf.String())
-
-	_, err = testGetObject(t, s3cli, "s3lock-test", "lock-obj")
-	require.ErrorContains(t, err, "The specified key does not exist")
-}
-
-func TestUnlockCmdAlreadyUnlocked(t *testing.T) {
-	s3cli := testNewS3Client(t)
-	testDeleteObject(t, s3cli, "s3lock-test", "lock-obj")
+	cfg, _ := config.LoadDefaultConfig(t.Context(), config.WithHTTPClient(hc))
+	s3cli := s3.NewFromConfig(cfg)
 
 	lockInfo := filepath.Join(t.TempDir(), "lock.info")
 	err := os.WriteFile(lockInfo, []byte(`{"Bucket":"s3lock-test","Key":"lock-obj","Id":"my-id","ETag":"\"my-etag\""}`), 0600)
@@ -57,9 +34,52 @@ func TestUnlockCmdAlreadyUnlocked(t *testing.T) {
 
 	var buf bytes.Buffer
 
+	httpmock.RegisterResponder(http.MethodGet, "https://s3lock-test.s3.us-east-1.amazonaws.com/lock-obj?x-id=GetObject", func(req *http.Request) (*http.Response, error) {
+		require.Equal(t, `"my-etag"`, req.Header.Get("If-Match"))
+		resp := httpmock.NewStringResponse(http.StatusOK, "my-id")
+		resp.Header.Set("x-amz-checksum-sha256", "kVQwchJF67pQ4+bz5XSdYrDB7HYv8636AALA6FQ3HFg=")
+		resp.Header.Set("x-amz-checksum-algorithm", "sha256")
+		return resp, nil
+	})
+
+	httpmock.RegisterResponder(http.MethodDelete, "https://s3lock-test.s3.us-east-1.amazonaws.com/lock-obj?x-id=DeleteObject", func(req *http.Request) (*http.Response, error) {
+		require.Equal(t, `"my-etag"`, req.Header.Get("If-Match"))
+		return httpmock.NewStringResponse(http.StatusOK, ""), nil
+	})
+
 	err = cmd.Run(&subcmd.Context{
 		S3:     s3cli,
 		Output: &buf,
+	})
+
+	require.NoError(t, err)
+	require.Empty(t, buf.String())
+}
+
+func TestUnlockCmdAlreadyUnlocked(t *testing.T) {
+	hc := &http.Client{}
+	httpmock.ActivateNonDefault(hc)
+	t.Cleanup(func() { httpmock.DeactivateNonDefault(hc) })
+
+	cfg, _ := config.LoadDefaultConfig(t.Context(), config.WithHTTPClient(hc))
+	s3cli := s3.NewFromConfig(cfg)
+
+	lockInfo := filepath.Join(t.TempDir(), "lock.info")
+	err := os.WriteFile(lockInfo, []byte(`{"Bucket":"s3lock-test","Key":"lock-obj","Id":"my-id","ETag":"\"my-etag\""}`), 0600)
+	require.NoError(t, err)
+
+	cmd := &subcmd.UnlockCmd{
+		LockFile: lockInfo,
+	}
+
+	httpmock.RegisterResponder(http.MethodGet, "https://s3lock-test.s3.us-east-1.amazonaws.com/lock-obj?x-id=GetObject", func(req *http.Request) (*http.Response, error) {
+		require.Equal(t, `"my-etag"`, req.Header.Get("If-Match"))
+		return httpmock.NewStringResponse(http.StatusNotFound, ""), nil
+	})
+
+	err = cmd.Run(&subcmd.Context{
+		S3:     s3cli,
+		Output: io.Discard,
 	})
 
 	require.ErrorIs(t, err, s3lock.ErrAlreadyUnlocked)
